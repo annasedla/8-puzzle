d079ffb93c13e649e974668d59402bbd6ad07ef4
Anna Sedlackova <axs1202@case.edu>
2018-09-10 03:18:57 -0400

    Created print state method and basic main method.

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,20 +1,51 @@
+import java.util.*;
+
 public class Puzzle {
 
     /*
-    Goal State
+    STATES
      */
 
+    //goal state
     int goalState [][]= {{0,1,2}, {3,4,5}, {6,7,8}};
 
-    /*
-    Current State
-     */
+    //current state
     int currentState [][] = new int [3][3];
 
     /*
-    Main Method
+    CONSTRUCTORS
+     */
+
+
+
+    /*
+    HELPER METHODS
+     */
+
+    public String printState (){
+        System.out.println("Current state of the puzzle: " + Arrays.toString(goalState));
+        return ("Current state of the puzzle: " + Arrays.toString(goalState));
+    }
+
+    /*
+    MAIN METHOD
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Puzzle puzzle = new Puzzle();
+        System.out.println("Welcome to Anna's Project!");
+
+        //Check for input
+        if (args.length > 0)
+        {
+            for (int i = 0; i < args.length; i++)
+            {
+                if (args[0].equals( "printState"))
+                    puzzle.printState();
+            }
+
+        }
+        else
+            System.out.println("No command line arguments found, check your inputs.");
     }
 }
