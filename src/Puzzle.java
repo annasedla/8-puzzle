fdbafdf7e4bc89a9be9562057062b78c7ad87baf
Anna Sedlackova <axs1202@case.edu>
2018-09-10 02:28:17 -0400

    Still testing things :(

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,3 +1,20 @@
 public class Puzzle {
-    
+
+    /*
+    Goal State
+     */
+
+    int goalState [][]= {{0,1,2}, {3,4,5}, {6,7,8}};
+
+    /*
+    Current State
+     */
+    int currentState [][] = new int [3][3];
+
+    /*
+    Main Method
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
