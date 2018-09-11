2a23cee135e7e657952e71ab47e8cb9235774bcb
Anna Sedlackova <axs1202@case.edu>
2018-09-11 00:15:31 -0400

    Added set state method and finished print method.

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Puzzle {
 
@@ -16,15 +17,42 @@ public class Puzzle {
     CONSTRUCTORS
      */
 
-
-
     /*
     HELPER METHODS
      */
 
-    public String printState (){
-        System.out.println("Current state of the puzzle: " + Arrays.toString(goalState));
-        return ("Current state of the puzzle: " + Arrays.toString(goalState));
+    //Print the state of the puzzle
+    public void printState (){
+
+        String print = "";
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (currentState[i][j] == 0)
+                    print += "b";
+                else
+                    print += currentState [i][j];
+            }
+        }
+        System.out.println("The state of the puzzle is:");
+        System.out.println(print);
+    }
+
+    //Set the state of the puzzle
+    public void setState(String s)
+    {
+        int k = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (s.charAt(k) == ('b')) {
+                    currentState[i][j] = 0;
+                } else {
+                    currentState[i][j] = s.charAt(k) - '0';
+                }
+                k++;
+            }
+        }
     }
 
     /*
@@ -34,16 +62,29 @@ public class Puzzle {
 
         Puzzle puzzle = new Puzzle();
         System.out.println("Welcome to Anna's Project!");
+        System.out.println();
+
+        puzzle.setState("b12345678");
+        puzzle.printState();
+
+        /*
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(new FileInputStream(fileName)));
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // process line
+            }
+        } finally {
+            br.close();
+        }*/
 
         //Check for input
-        if (args.length > 0)
-        {
-            for (int i = 0; i < args.length; i++)
-            {
-                if (args[0].equals( "printState"))
-                    puzzle.printState();
-            }
-
+        if (args.length > 0) {
+            if (args[0].equals( "printState"))
+                puzzle.printState();
+            if (args[0].equals("setState"))
+                puzzle.setState(args[1] + args[2] + args[3]);
         }
         else
             System.out.println("No command line arguments found, check your inputs.");
